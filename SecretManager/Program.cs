21958ee38669337e25af2e241d2d9bae9c7073c0
Shashanka <gudurajasekarreddy@gmail.com>
2024-05-15 00:03:46 +0530

    Added Missing Program.cs/main method

---
+++ SecretManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using SecretManager.Authenticator.DatabaseConnectors;
+using SecretManager.Models.Database;
+
+namespace SecretManager
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
